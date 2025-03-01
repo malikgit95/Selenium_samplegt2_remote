68c6496a92e8c95863d68e202a9cff1bcaa39507
malikgit95 <malikbasha0143@gmail.com>
2025-03-01 12:58:38 +0530

    first commit

---
+++ src/main/java/Selenium_samplegt2/Selenium_samplegt2/App.java
@@ -0,0 +1,10 @@
+package Selenium_samplegt2.Selenium_samplegt2;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/Selenium_samplegt2/Selenium_samplegt2/AppTest.java
@@ -0,0 +1,19 @@
+package Selenium_samplegt2.Selenium_samplegt2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
